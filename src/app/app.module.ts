2e746c7197d4615e426b642272d1c5e48381b252
shen <604389585@qq.com>
2018-11-02 09:42:40 +0800

    12

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,42 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { ballGameComponent } from './game/ballGame.component';
+import { mainComponent } from './main/main.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: '/main',
+  },
+  {
+    path: 'ballGame',
+    component: ballGameComponent,
+  },
+  {
+    path: 'main',
+    component: mainComponent,
+  },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ballGameComponent,
+    mainComponent
   ],
   imports: [
+    RouterModule.forRoot(routes),
     BrowserModule,
     FormsModule,
     HttpModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [RouterModule],
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
